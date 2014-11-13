8553de601b26377b0d13e01fea15ee60e2b909f4
zZED <xingxing0468@hotmail.com>
2014-11-13 18:56:32 +0800

    IPC MQ 1st Keep

--- ipc/MessageQueue/Client.cpp
+++ ipc/MessageQueue/Client.cpp
@@ -11,23 +11,30 @@ int main()
 				{
 					.mq_flags	= 0,
 					.mq_maxmsg	= 5,
-					.mq_msgsize	= 1024,
+					.mq_msgsize	= 20,
 				};
 	mqd_t mqfd = mq_open(ControlService::MQ_NAME.c_str(), O_WRONLY|O_CREAT, ControlService::PMODE, &sendMqAttr);
+	std::cout << "MQ Attr MSG SIZE: [" << sendMqAttr.mq_msgsize << "]" << std::endl;
+	std::cout << "MQ Attr MAX MSG: ["  << sendMqAttr.mq_maxmsg << "]" <<std::endl;
 	if(mqfd == -1)
 	{
 		perror("Open MQ Failed!!");
 		std::cout << "Error Number:[" << errno << "]" << std::endl;
 		exit(-1);
 	}
-	
-	if(0 != mq_timedsend(mqfd, sendStr.c_str(), strlen(sendStr.c_str() + 1), 0, &sendTimeOut))
+	int i = 0;
+	while(i < 20)
 	{
-		std::cout << "Sending Message Queue Failed!!" << std::endl;
-	}
-	else
-	{
-		std::cout << "Sending Mesaage Queue: [" << sendStr << "] Completed" << std::endl;
+		sendStr += "A";
+		if(0 != mq_timedsend(mqfd, sendStr.c_str(), strlen(sendStr.c_str()) + 1, 0, &sendTimeOut))
+		{
+			std::cout << "Sending Message Queue Failed!!" << std::endl;
+		}
+		else
+		{
+			std::cout << "Sending Mesaage Queue: [" << sendStr.c_str() << "] Completed" << std::endl;
+		}
+		i++;
 	}
 	mq_close(mqfd);
 	return 0;
